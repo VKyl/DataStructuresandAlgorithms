20ab41580023716174113dca5c2f1b700dc6fee0
vkyl <viktor.kyliukh@gmail.com>
2024-10-20 20:55:36 +0300

    Maze fixed

--- Pr6/labyrinth/maze/main.py
+++ Pr6/labyrinth/maze/main.py
@@ -7,14 +7,14 @@ from bfs import BFS
 def generate_graph(maze):
     graph = Graph()
     for row in range(maze.y_size):
-        for col in range(maze.x_size - 1):
+        for col in range(maze.x_size):
             if maze.maze[row][col].side(1):
                 node1 = row * maze.x_size + col
                 node2 = row * maze.x_size + col + 1
                 graph.add_edge(node1, node2)
-            if row > 0 and maze.maze[row - 1][col].side(2):
+            if maze.maze[row][col].side(2):
                 node1 = row * maze.x_size + col
-                node2 = (row - 1) * maze.x_size + col
+                node2 = (row + 1) * maze.x_size + col
                 graph.add_edge(node1, node2)
     return graph
 
@@ -33,8 +33,9 @@ def bfs_solve(maze):
     print(bfs.path_to(end))
 
 
+
 if __name__ == '__main__':
-    maze = Maze(3, 2)
+    maze = Maze(3, 3)
     print(maze)
     dfs_solve(maze)
     bfs_solve(maze)
